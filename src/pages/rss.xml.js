43dca6d8984e6d3d4bd7bef30b2fea0727bec97f
坂本 <sakamoto@seven-inc.co.jp>
2023-08-04 17:24:30 +0900

    upd

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,7 +4,7 @@ export async function get() {
   return rss({
     title: 'Astro学習者 | ブログ',
     description: 'Astroを学ぶ旅',
-    site: 'https://my-blog-site.netlify.app',
+    site: 'https://velvety-concha-94f9a3.netlify.app',
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>ja-jp</language>`,
   });
